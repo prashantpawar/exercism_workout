2b0224a482c1c07d7da832067db7eab1cc5151c5
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-10-30 22:34:00 -0400

    Fully solved space age example

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -1,15 +1,17 @@
 defmodule SpaceAge do
-  # @type planet :: :mercury | :venus | :earth | :mars | :jupiter | :saturn | :neptune | :uranus
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter | :saturn | :neptune | :uranus
 
-  @constants planets %{
+  @seconds_in_year 31557600
+
+  @planets %{
     :mercury => 0.2408467,
     :venus => 0.61519726,
     :earth => 1,
     :mars => 1.88088158,
     :jupiter => 11.862615,
     :saturn => 29.447498,
-    :neptune => 84.016846,
-    :uranus => 164.79132
+    :uranus => 84.016846,
+    :neptune => 164.79132
   }
 
   @doc """
@@ -19,7 +21,7 @@ defmodule SpaceAge do
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
 
-
+    seconds / (@seconds_in_year * @planets[planet])
 
   end
 end
